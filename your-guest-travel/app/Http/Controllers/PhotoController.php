cf0d968927f20b10f110d5640297ef1a931fdd09
Dione <arantes.designergrafico@gmail.com>
2023-12-06 17:39:26 +0000

    Corrigindo envio de imagem

---
+++ your-guest-travel/app/Http/Controllers/PhotoController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Photo;
+use Illuminate\Http\Request;
+
+class PhotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+       $request->validate([
+           'image'=> 'required|image|mimes:jpg,png,jpeg,gif, svg|max:2048',
+       ]);
+
+       $name = $request->file('image')->getClientOriginalName();
+       $request->file('image')->store('public/images');
+
+       $picture = new Photo;
+       $picture->name = $name;
+       $picture->path = $request->file('image')->hashName();
+       $picture->save();
+       return redirect()->back()->with('status', 'Upload bem sucedido');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Photo $photo)
+    {
+        //
+    }
+}
